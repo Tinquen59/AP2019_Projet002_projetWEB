2b45f043f30ce6c624e1523e8e9ad30d94c428ac
Quentin.A <quentin.andrieu1@viacesi.fr>
2020-04-10 18:40:12 +0200

    Ajout du message d'erreur pour la plupart des input

--- assets/js/formulaire.js
+++ assets/js/formulaire.js
@@ -1,43 +1,69 @@
-/*
-let casErreurs = [
+let typeErrors = [
     {
-        idElt: "nom",
-        reponseErreur: "un nom"
+        regexVerif: /[a-z]{2}/i,
+        nameError: 'un nom valide'
+    },
+    {
+        regexVerif: /[a-z]{2}/i,
+        nameError: 'un prenom valide'
+    },
+    {
+        regexVerif: /[a-z]+/i,
+        nameError: 'un nom de société valide'
+    },
+    {
+        regexVerif: /[a-z]{3}/i,
+        nameError: 'un nom de poste valide'
+    },
+    {
+        regexVerif: /\d+ [a-z]+/i,
+        nameError: 'une adresse'
+    },
+    {
+        regexVerif: /^\d{10}$/i,
+        nameError: 'un numéro de téléphone valide'
+    },
+    {
+        regexVerif: /^\d{10}$/i,
+        nameError: 'un numéro de téléphone valide'
+    },
+    {
+        regexVerif: /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/,
+        nameError: 'un email valide'
     }
 ]
 
-let divFormGroup = document.querySelectorAll('div[class="col-sm-8"]')
-let testRequired = document.querySelectorAll('input')
-let formulaire = document.querySelector('form')
 
-console.log(casErreurs[0].reponseErreur)
-
-console.log(testRequired[1])
-console.log(divFormGroup[0])
-
-formulaire.addEventListener('submit', function (e) {
+document.querySelector('form').addEventListener('submit', function (e) {
     e.preventDefault()
 
-    console.log(testRequired[1].value)
+    let divFormGroup = document.querySelectorAll('div .divError')
 
-    if (testRequired[1].value.length == 0) {
-        testRequired[1].focus()
-        testRequired[1].style.borderColor = "red"
-        divFormGroup[0].innerHTML += `<p class="text-danger text-lowercase">Veuillez indiquer un nom valide</p>`
+    for (let i = 0; i < divFormGroup.length; i++) {
+        if (!typeErrors[i].regexVerif.test(divFormGroup[i].querySelector('input').value)) {
+            divFormGroup[i].querySelector('input').style.borderColor = 'red'
+            divFormGroup[i].querySelector('.messageError').innerHTML = `<p class="text-danger m-0">Veuillez indiquer ${typeErrors[i].nameError}</p>`
+        } else {
+            divFormGroup[i].querySelector('input').style.borderColor = 'green'
+            divFormGroup[i].querySelector('.messageError').innerHTML = ''
+        }
     }
-})*/
 
 
-let inputFormulaire = document.querySelectorAll('input')
-let formulaire = document.querySelector('form')
-
-formulaire.addEventListener('submit', function (e) {
-    e.preventDefault()
-
-    let inputRequireds = inputFormulaire.required
-
-
-    if (inputRequireds == true) {
-        console.log(inputRequireds)
-    }
 })
+
+
+let xhr = new XMLHttpRequest()
+
+xhr.open('GET', 'https://unpkg.com/codes-postaux@3.3.0/codes-postaux.json')
+
+xhr.onload = function () {
+    let resultatCodePostaux = JSON.parse(this.responseText)
+    resultatCodePostaux.forEach(function (resultatCodePostal) {
+        if (document.getElementById('codePostal').value === resultatCodePostal.codePostal) {
+            document.getElementById('ville').innerHTML += `<option>${resultatCodePostal.nomCommune}</option>`
+        }
+    })
+}
+
+xhr.send()
